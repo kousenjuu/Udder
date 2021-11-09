2a5a7dc965ec485000543f74c0e6ade203fb1ff0
Reagan Blank <kousenjuu@gmail.com>
2021-11-09 13:00:42 -0600

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Reagan Blank on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

262588202eedeb8af741ae8bd7754167a8307241
Reagan Blank <kousenjuu@gmail.com>
2021-11-09 13:19:06 -0600

    Add label and images

---
+++ Udder/Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Reagan Blank on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
